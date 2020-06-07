a79c24daed9eba31b30f2cfd22c8e90168e2b5e2
androwsobhy <65551547+androwsobhy@users.noreply.github.com>
2020-06-07 18:57:47 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+let products = [
+    {name: "Pepsi",quantity: 100,type: "Unit",},
+    {name: "Tomato",quantity: 50,type: "Kilo",},   ];
+   
+ console.log(products)
+    document.getElementById("any-id").innerHTML = products;
+
+
+
+
+    function addProducts() {
+        products.push(name: "Chocolate",quantity: 20,type: "unit",);
+
+        document.getElementById("pr").innerHTML = products;
+
+
+<button onclick="addProducts()">add product</button>
